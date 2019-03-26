de8ad6edf792331572d75af3a77f076a103d7501
xiaoshuaiyao <1042799677@qq.com>
2019-03-26 09:25:37 +0800

    首页头部写完

---
+++ H1824/myapp/src/components/header.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="header">
+    <div class="header_left">
+      <img src="../images/bg.png" alt>
+    </div>
+    <div class="header_right">
+      <span class="iconfont">&#xe606;</span>
+    </div>
+    <ul class="header_dis">
+      <li v-for="(item,index) in diss">{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data(){
+    return{
+        diss:["产品","解决方案","客户案例","服务中心"]
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.header {
+  position: fixed;
+  left: 0;
+  top: 0;
+  display: flex;
+  width: 100%;
+  height: 56px;
+  justify-items: center;
+  padding: 0 20px;
+  align-items: center;
+}
+.header_left {
+  width: 250px;
+  height: 56px;
+}
+.header_left > img {
+  width: 120px;
+  height: 42px;
+  padding: 5px;
+}
+.header_right {
+  width: 93px;
+  height: 56px;
+  display: flex;
+  align-items: center;
+}
+.header_right > span {
+  color: green;
+  margin-left: 60px;
+}
+.header_dis{
+  position: absolute;
+  width: 100%;
+  height: 200px;
+  left: 0;
+  top: 56px;
+  margin: 20px;
+}
+.header_dis{
+  line-height: 30px;
+}
+</style>
--- H1824/myapp/src/views/Home.vue
+++ H1824/myapp/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+import Header from '../components/header'
+import Common from '../components/common.css'
 export default {
   name: 'home',
   components: {
-    HelloWorld
+      Header,
+      Common
   }
 }
 </script>
